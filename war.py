bcca64705b04fbcdd459ad82e6fd0ef8c47bea53
Jacob Essig-Beatty <jessigbeatty@gmail.com>
2020-08-30 20:25:49 -0500

    added features to war.py, introduced some weird bug that he's trying to fix now

--- war.py
+++ war.py
@@ -3,6 +3,32 @@ import player
 
 # implements the game 'War'
 
+# one turn
+def play_turn():
+    global pot; pot = []
+    p1_played = player1.hand.pop()
+    p2_played = player2.hand.pop()
+    pot.append(p1_played)
+    pot.append(p2_played)
+    print("Player 1 plays: ", end='')
+    p1_played.show()
+    print("Player 2 plays: ", end='')
+    # it breaks here on the second time through looping and i don't get whyyyy
+    p2_played.show()
+    return p1_played, p2_played
+
+# compare card values
+def compare(p1, p2):
+    if p1.value > p2.value:
+        print("Player 1 wins.")
+        player1.hand.append(pot)
+    elif p1.value < p2.value:
+        print("Player 2 wins.")
+        player2.hand.append(pot)
+    else:
+        print("This means WAR!!!")
+        # implement WAR
+
 player1 = player.Player()
 player2 = player.Player()
 
@@ -15,17 +41,8 @@ while len(my_deck.cards) != 0:
     player1.draw(my_deck)
     player2.draw(my_deck)
 
-# one turn
-p1_played = player1.hand.pop()
-p2_played = player2.hand.pop()
-print("Player 1 plays: ", end='')
-p1_played.show()
-print("Player 2 plays: ", end='')
-p2_played.show()
+# plays hands in a loop
+while len(player1.hand) != 0 and len(player2.hand) != 0:
+    p1, p2 = play_turn()
+    compare(p1, p2)
 
-if p1_played.value > p2_played.value:
-    print("Player 1 wins.")
-elif p1_played.value < p2_played.value:
-    print("Player 2 wins.")
-else:
-    print("This means WAR!!!")
